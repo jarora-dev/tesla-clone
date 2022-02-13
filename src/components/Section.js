a8b6a8b2d01cec5b8c3ac9281442541c2287947d
Jatin Arora <jarora15@myseneca.ca>
2022-02-12 20:36:08 -0500

    Completed main section

--- src/components/Section.js
+++ src/components/Section.js
@@ -8,6 +8,17 @@ function Section() {
             <h1>Model S</h1>
             <p>Order Online for touchless delivery</p>
         </ItemText>
+        <Button>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow src="/images/down-arrow.svg"/>
+        </Button>
     </Wrap>
   )
 }
@@ -21,9 +32,48 @@ const Wrap = styled.div`
     background-size: cover;
     background-position: center;
     background-repeat: no-repeat;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; //vertical
+    align-items : center;
 `
 
 const ItemText = styled.div`
     padding-top: 15vh;
     text-align: centre;
 `
+
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+
+const LeftButton = styled.div`
+    background-color: rgba(23,26,32,0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    font-size: 14px;
+    cursor: pointer;
+    text-transform: uppercase;
+    margin: 8px;
+`
+
+const RightButton = styled(LeftButton)`
+    background-color: white;
+    opacity: 0.65;
+    color: black;
+`
+
+const DownArrow = styled.img`
+    margin-top: 20px;
+    height: 40px;
+    animation animateDown infinite 1.5s;
+    overflow: hidden;
+`
+const Button = styled.div``
